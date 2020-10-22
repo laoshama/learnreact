4132fae07232fe2cfaf9f003ef3a5be434e0ad9d
laoshama <56189059+laoshama@users.noreply.github.com>
2020-10-22 16:43:33 +0800

    首次提交

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React , { Component } from 'react';
+
+export default class App extends Component {
+    constructor() {
+      super()
+      this.state = {
+      }
+    }
+    render() {
+      return (
+        <div>
+          <h2>hello react</h2>
+        </div>
+      )
+    }
+  }
